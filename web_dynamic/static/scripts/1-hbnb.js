6b4f2902c03cb73d56cc7d0e24d402ee1bcb49f7
langatfestus <langatfestus94@gmail.com>
2023-10-09 23:01:31 +0300

    completed

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,28 @@
+$(() => {
+  const checkedAmenities = {};
+  let displayAmenities = [];
+  $('.amenities .popover input').change(function () {
+    if ($(this).prop('checked')) {
+      checkedAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete checkedAmenities[$(this).attr('data-id')];
+    }
+    displayAmenities = Object.values(checkedAmenities).sort();
+    if (displayAmenities.length != 0)
+      $('.amenities h4').text(displayAmenities.join(', '));
+    else
+      $('.amenities h4').html("&nbsp;");
+
+    // console.log(checkedAmenities);
+
+let amenitiesChecked = {};
+$(() => {
+  $('input[type=checkbox]').click(function () {
+    if (this.checked) {
+      amenitiesChecked[this.dataset.id] = this.dataset.name;
+    } else {
+      delete amenitiesChecked[this.dataset.id];
+    }
+    $('.amenities h4').text(Object.values(amenitiesChecked).join(', '));
+  });
+});
